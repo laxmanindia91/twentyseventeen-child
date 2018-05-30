308c568abf54bfd68fdee34620f00b4c3683c4fc
Laxman <laxman@netscriptindia.com>
2018-05-30 13:51:04 +0530

    nav menu

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($){
 	
+	jQuery('#user_login').attr("disabled", false);
+	
     $(".app_coupon_code").click(function(){
     	var coupon = $('.apply_coupon').val();
     	//$('.apply_coupon').val('');
@@ -20,7 +22,7 @@ jQuery(document).ready(function($){
 	$('.next_coupon').click(function(){
 		//alert('coupon');
 		//$(".tab_coupon").click();
-		document.getElementById('tab_coupon').click();
+		//document.getElementById('tab_coupon').click();
 		//$('.tab_coupon').click();
 		//$('#custom_billing_form').show('fast');
 	});
